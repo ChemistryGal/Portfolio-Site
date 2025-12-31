dc7f12add56201d337f20ff2fb94d2f101d0a8d6
ChemistryGal <khancock524@gmail.com>
2025-12-31 12:35:29 -0700

    adjusted router issue

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename = "/Portfolio-Site/">
       <Routes>
         <Route path = "/" element ={<Home />}/>
         <Route path = "*" element ={<NotFound />}/>
